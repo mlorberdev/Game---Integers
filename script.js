ead232cb625625a2329eee3da02dcf0898147b21
mlorber <87738509+mlorberdev@users.noreply.github.com>
2023-07-09 09:30:55 -0400

    begin scripting

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 !(function () {
-  // document.getElementById("checkbox").addEventListener("change", () => document.body.classList.toggle("dark"));
+
+  // VARS
+  let tt = document.getElementById("time");
+
+
+  // LISTENERS
+  document.getElementById("multiplication").addEventListener("click", () => {
+    document.getElementById("landing-page").style.display = "none";
+    document.getElementById("options-page").style.display = "inherit";
+  });
+  document.getElementById("addition").addEventListener("click", () => {
+    document.getElementById("landing-page").style.display = "none";
+    document.getElementById("options-page").style.display = "inherit";
+  });
+  document.getElementById("begin").addEventListener("click", () => {
+    document.getElementsByName("time").forEach(t => { if (t.checked) tt.innerHTML = 60 * t.value; });
+    document.getElementById("options-page").style.display = "none";
+    document.getElementById("working-page").style.display = "inherit";
+  });
+
 })();
