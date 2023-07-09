e959e89ad1e8e12d11514ff259310009f6f069be
mlorber <87738509+mlorberdev@users.noreply.github.com>
2023-07-09 12:32:35 -0400

    added fonts

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 !(function () {
 
-  // VARS
-  let tt = document.getElementById("time");
+  ////////////// VARS
+  const tt = document.getElementById("timer");
+  let t;
 
 
-  // LISTENERS
+  ////////////// LISTENERS
+
+  // MULT-DIV
   document.getElementById("multiplication").addEventListener("click", () => {
     document.getElementById("landing-page").style.display = "none";
     document.getElementById("options-page").style.display = "inherit";
   });
+  // ADD-SUB
   document.getElementById("addition").addEventListener("click", () => {
     document.getElementById("landing-page").style.display = "none";
     document.getElementById("options-page").style.display = "inherit";
   });
+  // START TIMER
   document.getElementById("begin").addEventListener("click", () => {
-    document.getElementsByName("time").forEach(t => { if (t.checked) tt.innerHTML = 60 * t.value; });
+    document.getElementsByName("time").forEach(c => {
+      if (c.checked) {
+        t = 60 * c.value;
+      }
+    });
     document.getElementById("options-page").style.display = "none";
     document.getElementById("working-page").style.display = "inherit";
+    timer();
   });
+
+  ///////////// TIMER
+  function timer() {
+    tt.innerHTML = t;
+    const int = setInterval(() => { t > 0 ? tick() : clearInterval(int); }, 1000);
+    const tick = () => { t--; tt.innerHTML = t; }
+  }
+
+  
 
 })();
