a3e77155fe0c7be2fb31448b38c30bee19bae291
mlorber <87738509+mlorberdev@users.noreply.github.com>
2023-07-10 19:52:12 -0400

    added sounds

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
 !(function () {
 
-  ////////////// VARS
-  const tt = document.getElementById("timer");
-  let t;
 
+  const right = new Audio("./right.mp3");
+  const wrong = new Audio("./wrong.mp3");
+  
 
-  ////////////// LISTENERS
+  // DARK MODE TOGGLE
+  // let lights = false;
+  // document.getElementById("lights").addEventListener("click", () => {
+  //   const r = document.querySelector(':root');
+  //   r.style.setProperty('--lite', lights === true ? '#fff' : '#000');
+  //   r.style.setProperty('--dark', lights === true ? '#000' : '#fff');
+  //   lights === true ? lights = false : lights = true;
+  // });
 
-  // MULT-DIV
-  document.getElementById("multiplication").addEventListener("click", () => {
-    document.getElementById("landing-page").style.display = "none";
-    document.getElementById("options-page").style.display = "inherit";
-  });
-  // ADD-SUB
-  document.getElementById("addition").addEventListener("click", () => {
-    document.getElementById("landing-page").style.display = "none";
-    document.getElementById("options-page").style.display = "inherit";
-  });
   // START TIMER
   document.getElementById("begin").addEventListener("click", () => {
-    document.getElementsByName("time").forEach(c => {
-      if (c.checked) {
-        t = 60 * c.value;
-      }
-    });
-    document.getElementById("options-page").style.display = "none";
-    document.getElementById("working-page").style.display = "inherit";
-    timer();
+    const pr = document.getElementById("progress");
+    // set attribute max on pr for duration
+    const int = setInterval(() => { pr.value < 60 ? pr.value++ : clearInterval(int); }, 1000);
+    gameStart();
   });
 
-  ///////////// TIMER
-  function timer() {
-    tt.innerHTML = t;
-    const int = setInterval(() => { t > 0 ? tick() : clearInterval(int); }, 1000);
-    const tick = () => { t--; tt.innerHTML = t; }
+  // JUST SIGNS
+  function gameStart() {
+    const qq = document.getElementById("qq");
+    let p = document.getElementById("pos");
+    let n = document.getElementById("neg");
+    
+    function askQ() {
+      let a = Math.floor(Math.random() * 10);
+      let b = Math.floor(Math.random() * 10);
+      let o = Math.random() < .5 ? "-" : "+";
+      let s = 0 === "-" ? a - b : a + b; console.log(s);
+      qq.innerHTML = `${a.toString()} ${o} ${b.toString()}`;
+      p.addEventListener("click", () => {
+        s > 0 ? right.play() : wrong.play();
+      });
+      n.addEventListener("click", () => {
+        s < 0 ? right.play() : wrong.play();
+      });
+    } askQ();
   }
-
-  
 
 })();
