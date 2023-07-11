7ce46804daee0853077303fa4bbe74bf85950589
mlorber <87738509+mlorberdev@users.noreply.github.com>
2023-07-10 22:38:18 -0400

    iterative save

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 !(function () {
 
-
+  const begin = document.getElementById("begin");
   const right = new Audio("./right.mp3");
   const wrong = new Audio("./wrong.mp3");
+  let question, answer, diff = "med", n = 1, arr = [];
   
 
   // DARK MODE TOGGLE
@@ -15,7 +16,7 @@
   // });
 
   // START TIMER
-  document.getElementById("begin").addEventListener("click", () => {
+  begin.addEventListener("click", () => {
     const pr = document.getElementById("progress");
     // set attribute max on pr for duration
     const int = setInterval(() => { pr.value < 60 ? pr.value++ : clearInterval(int); }, 1000);
@@ -24,23 +25,34 @@
 
   // JUST SIGNS
   function gameStart() {
+    begin.style.visibility = document.getElementById("subtitle").style.visibility = "hidden";
     const qq = document.getElementById("qq");
-    let p = document.getElementById("pos");
-    let n = document.getElementById("neg");
+    document.getElementById("pos").addEventListener("click", ()=> ans(1));
+    document.getElementById("neg").addEventListener("click", ()=> ans(-1));
     
-    function askQ() {
-      let a = Math.floor(Math.random() * 10);
-      let b = Math.floor(Math.random() * 10);
-      let o = Math.random() < .5 ? "-" : "+";
-      let s = 0 === "-" ? a - b : a + b; console.log(s);
-      qq.innerHTML = `${a.toString()} ${o} ${b.toString()}`;
-      p.addEventListener("click", () => {
-        s > 0 ? right.play() : wrong.play();
-      });
-      n.addEventListener("click", () => {
-        s < 0 ? right.play() : wrong.play();
-      });
-    } askQ();
+    function ask() {
+      let a = Math.floor(Math.random() * 10) * (Math.random() < .5 ? -1 : 1);
+      let b = Math.floor(Math.random() * 10); console.log(b);
+      if (diff === "med") b *= Math.random() < .5 ? -1 : 1;
+      let op = Math.random() < .5 ? "-" : "+";
+      answer = op === "+" ? a + b : a - b;
+      question = `${a} ${op} ${b}`;
+      if (op === "-" && b < 0 && Math.random() < .5) question = `${a} ${op} (${b})`;
+      
+      // arr.push(n, `${question} = ${answer}`);
+      console.log(n, `${question} = ${answer}`);
+      qq.innerHTML = question;
+      n++;
+    } ask();
+
+    function ans(val) {
+      if ((answer >= 0 && val === 1) || answer < 0 && val === -1) {
+        right.play();
+      } else {
+        wrong.play();
+      }
+      ask();
+    }
   }
 
 })();
