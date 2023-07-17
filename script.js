a13ac1eba93ee13ff280e1f9a579b16c96481a6c
mlorber <87738509+mlorberdev@users.noreply.github.com>
2023-07-16 22:53:39 -0400

    iterative save

--- script.js
+++ script.js
@@ -1,65 +1,80 @@
 !(function () {
 
-  const burger = document.getElementById("hamburger");
+
+  const intro = document.getElementById("intro_page");
+  const practice = document.getElementById("practice");
+  const check = document.getElementById("ready_check");
+  const pr = document.getElementById("progress");
   const stats = document.getElementById("stats");
-  const begin = document.getElementById("start");
+  let op;
+  document.querySelectorAll("input[name=operation]").forEach(o => { if (o.checked) op = o.value; });
   const right = new Audio("./right.mp3");
   const wrong = new Audio("./wrong.mp3");
   const arr = [];
   let sound = document.getElementById("sound");
   let vibes = document.getElementById("vibes");
-  let question,
-    answer,
-    diff = "med",
+  let diff = "med",
     n = 1,
-    tt;
-  
-  // OPTIONS
-  burger.addEventListener("click", function () {
-    const opts = document.getElementById("options");
-    if (this.classList.contains("rotate")) {
-      this.classList.remove("rotate");
-      opts.style.left = 0;
-      open = true;
-    } else {
-      this.classList.add("rotate");
-      opts.style.left = "100vw";
-      open = false;
-    }
+    tt = 60;
+
+  // FULLSCREEN
+  // document.getElementById("fullscreen").addEventListener("click", () => { document.body.requestFullscreen(); });
+
+  // EXIT INTRO
+  document.getElementById("start").addEventListener("click", function () {
+    intro.classList.add("none");
+    options.classList.remove("none");
   });
 
-  // TIMER
-  begin.addEventListener("click", () => {
-    const pr = document.getElementById("progress");
-    document.querySelectorAll("[name=timer]").forEach(t => {
-      if (t.checked) {
-        tt = t.value;
-        pr.setAttribute("max", tt);
-      }
-    });
-    const int = setInterval(() => { pr.value < tt ? pr.value++ : dostats(int) }, 1000);
-    gameStart();
+  // EXIT OPTIONS
+  document.getElementById("set_options").addEventListener("click", function () {
+    options.classList.add("none");
+    check.classList.remove("none");
   });
 
-  // JUST SIGNS
-  function gameStart() {
-    begin.style.visibility = document.getElementById("subtitle").style.visibility = "hidden";
+  // READY CHECK
+  document.getElementById("ready_check").addEventListener("click", function () {
+    check.classList.add("none");
+    practice.classList.remove("none");
+  });
+
+  // PRACTICE START
+  document.getElementById("lets_go").addEventListener("click", function () {
+    check.classList.add("none");
+    practice.classList.remove("none");
+    const int = setInterval(() => { pr.value < tt ? pr.value++ : stopTimer(int) }, 1000);
     const qq = document.getElementById("question");
-    document.getElementById("pos").addEventListener("click", () => ans(1));
-    document.getElementById("zer").addEventListener("click", () => ans(0));
-    document.getElementById("neg").addEventListener("click", ()=> ans(-1));
-    
+    document.getElementById("nn").addEventListener("click", () => ans(-1));
+    // document.getElementById("zz").addEventListener("click", () => ans(0));
+    document.getElementById("pp").addEventListener("click", () => ans(1));
+
+
     function ask() {
       let a = Math.floor(Math.random() * 10) * (Math.random() < .5 ? -1 : 1);
       let b = Math.floor(Math.random() * 10);
+      let Q, A, oo;
       if (diff === "med") b *= Math.random() < .5 ? -1 : 1;
-      let op = Math.random() < .5 ? "-" : "+";
-      answer = op === "+" ? a + b : a - b;
-      question = `${a} ${op} ${b}`;
-      if (op === "-" && b < 0 && Math.random() < .5) question = `${a} ${op} (${b})`;
-      
-      arr.push(`${question} = ${answer}`);
-      qq.innerHTML = question;
+      console.log(op);
+      switch (op) {
+        case "add": Q = `${a} + ${b}`; A = a + b; break;
+        case "sub": Q = `${a} - ${b}`; A = a - b; break;
+        case "aos":
+          oo = Math.random() < .5 ? "-" : "+";
+          Q = `${a} ${oo} ${b}`;
+          A = oo === "-" ? A - B : A + B;
+          break;
+        case "mmm": Q = `${a} * ${b}`; A = a * b; break;
+        case "ddd": Q = `${a} / ${b}`; A = a / b; break;
+        case "mod":
+          oo = Math.random() < .5 ? "*" : "/";
+          Q = `${a} ${oo} ${b}`;
+          A = oo === "*" ? A * B : A / B;
+          break;
+        default: 
+      }
+
+      arr.push(`${Q} = ${A}`);
+      qq.innerHTML = Q;
       n++;
     } ask();
 
@@ -74,19 +89,21 @@
       }
       ask();
     }
-  }
 
-  // UPDATE STATS PAGE
-  function dostats(int) {
-    clearInterval(int);
-    const frag = new DocumentFragment();
-    for (let i = 0; i < arr.length - 1; i += 2) {
-      let ele = document.createElement("li");
-      ele.textContent = `${arr[i]} ${arr[i + 1]}`;
-      frag.appendChild(ele);
+    // STOP TIMER AND UPDATE STATS PAGE
+    function stopTimer(int) {
+      clearInterval(int);
+      const frag = new DocumentFragment();
+      for (let i = 0; i < arr.length - 1; i += 2) {
+        let ele = document.createElement("li");
+        ele.textContent = `${arr[i]} ${arr[i + 1]}`;
+        frag.appendChild(ele);
+      }
+      document.getElementById("list").appendChild(frag);
+      setTimeout(() => stats.style.left = 0, 500);
     }
-    document.getElementById("list").appendChild(frag);
-    setTimeout(() => stats.style.left = 0, 500);
-  }
+  });
+
+
 
 })();
